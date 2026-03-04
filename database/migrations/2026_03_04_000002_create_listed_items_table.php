c977b7ce3a80526563492bd2226c4a89bc8ed33a
nicotinelove <nicotinesukisuki@proton.me>
2026-03-04 16:41:59 +0900

    migrationファイルとmodelファイルを作り直した

---
+++ database/migrations/2026_03_04_000002_create_listed_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listed_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('item_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->boolean('is_opend')->nullable();
+            $table->string('exchange_area');
+            $table->integer('is_trading');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listed_items');
+    }
+};
