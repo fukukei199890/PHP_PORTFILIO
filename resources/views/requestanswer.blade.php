46598afbef3c2e54a30c9930e731bdb08e118fd0
tarochiku2002 <tarochiku2002@gmail.com>
2026-03-06 12:57:24 +0900

    リクエストアンサーページ作成（HTML）

--- resources/views/requestanswer.blade.php
+++ resources/views/requestanswer.blade.php
@@ -1,15 +1,44 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<x-user-layout>
+    
+        <div class="min-h-screen bg-gray-100 flex flex-col justify-between">
+            <p>お相手が見つかりました</p>
+            {{-- 中央エリア --}}
+            <div class="flex flex-1 items-center justify-center px-6 pb-24">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+                {{-- カード --}}
+                <div class="bg-[#e8dddd] border border-[#c9a9a9] 
+                    rounded-[40px] 
+                    w-full max-w-md 
+                    p-10 text-center shadow-sm">
 
-    <title>Laravel</title>
-</head>
+                    {{-- フォーム --}}
+                    <!-- route関数はエイリアス（別名）phpのasみたいなもので指定してそのURLを呼び出す -->
+                    <form method="get" action="{{ route('rating') }}" class="space-y-6">
+                        @csrf
 
-<body>
-    <h2>お相手が見つかりました！</h2>
-</body>
+                        {{-- はい --}}
+                        <button type="submit"
+                            name="action"
+                            value="yes"
+                            class="w-40 py-3 
+                               bg-gray-200 border border-black 
+                               rounded-xl 
+                               hover:bg-gray-300 transition">
+                            はい
+                        </button>
 
-</html>
+                    </form>
+
+                    <form method="get" action="{{ route('message') }}" class="space-y-6">
+                        {{-- いいえ --}}
+                        <button type="submit"
+                            name="action"
+                            value="no"
+                            class="w-40 py-3 
+                               bg-gray-200 border border-black 
+                               rounded-xl 
+                               hover:bg-gray-300 transition">
+                            いいえ
+                        </button>
+                    </form>
+</x-user-layout>
