45d180a5a6311334327673f66475cd70d7364812
Keisuke Fukuda <futianqijie560@gmail.com>
2026-03-07 13:22:20 +0900

    mypage review　データベースで接続しました

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -1,8 +1,5 @@
 <x-user-layout>
     <!-- ログイン情報 -->
-    <p>{{Auth::user()->id }}</p>
-    <p>{{Auth::user()->name }}</p>
-    <p>{{Auth::user()->email }}</p>
-    <p>{{Auth::user()->password }}</p>
-    <p>{{Auth::user()}}</p>
+    <p>{{ Auth::user()->name }}</p>
+    <p>{{ $score }}</p>
 </x-user-layout>
