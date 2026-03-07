749ad0cbba59830e0e24cbad740f5db2964cf06d
Keisuke Fukuda <futianqijie560@gmail.com>
2026-03-07 11:48:55 +0900

    mypadebladeを編集しました

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -1,18 +1,8 @@
 <x-user-layout>
-    <!DOCTYPE html>
-    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>マイページ</title>
-    </head>
-
-    <body>
-        <h2>ユーザ名</h2>
-    </body>
-
-    </html>
-
+    <!-- ログイン情報 -->
+    <p>{{Auth::user()->id }}</p>
+    <p>{{Auth::user()->name }}</p>
+    <p>{{Auth::user()->email }}</p>
+    <p>{{Auth::user()->password }}</p>
+    <p>{{Auth::user()}}</p>
 </x-user-layout>
