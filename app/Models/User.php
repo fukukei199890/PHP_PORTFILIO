0c7bb0579371cab7f2bf587d96dc28111b0bdf04
nicotinelove <nicotinesukisuki@proton.me>
2026-03-10 11:04:10 +0900

    トーク一覧を作りました

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function listed_items()
+    {
+        return $this->hasMany(ListedItem::class);
+    }
 }
