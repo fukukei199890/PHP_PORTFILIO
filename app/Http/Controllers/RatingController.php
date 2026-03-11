7e85f7609af8a6899b85fda099f6ed256f1a4199
tarochiku2002 <tarochiku2002@gmail.com>
2026-03-11 10:50:40 +0900

    評価ページ作成とグローバルナビゲーションの上の余白消しました

--- app/Http/Controllers/RatingController.php
+++ app/Http/Controllers/RatingController.php
@@ -7,14 +7,12 @@ use App\Models\User;
 
 class RatingController extends Controller
 {
-    public function index(
-        // $id
-    )
+    public function index($id)
     {
     // 送られてきた $id (相手のユーザーID) で検索
-    // $user = User::findOrFail($id);
+    $user = User::findOrFail($id);
 
     // 取得した相手の情報を 'user' という名前でViewに渡す ['user' => $user]
-    return view('rating');
+    return view('rating',['user' => $user]);
     }
 }
