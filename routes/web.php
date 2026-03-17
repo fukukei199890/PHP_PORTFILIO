2bf0589d46cf6ab293dea7fede0e686ed0851865
Keisuke Fukuda <futianqijie560@gmail.com>
2026-03-17 10:30:23 +0900

    コンフリクト解消のコミット

--- routes/web.php
+++ routes/web.php
@@ -212,7 +212,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::patch('/notifications/{id}/read',[MessageReceivedController::class,'read'])->name('messageReceived.read');
+    Route::patch('/notifications/{id}/read', [MessageReceivedController::class, 'read'])->name('messageReceived.read');
 });
 
 require __DIR__ . '/auth.php';
