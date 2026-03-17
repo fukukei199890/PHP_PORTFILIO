d72d35c894e5f5d621fe442ecdc587d62f7d779a
nicotinelove <nicotinesukisuki@proton.me>
2026-03-17 09:46:04 +0900

    user.baldeの通知数をcount()で数えるようにしました

--- resources/views/messageReceived.blade.php
+++ resources/views/messageReceived.blade.php
@@ -1,7 +1,8 @@
 <x-user-layout>
-    <ul>
+    {{-- <ul>
         @foreach (Auth::user()->unreadNotifications as $notification)
             <li>{{ $notification->sender_name }}からのメッセージがあります</li>
         @endforeach
-    </ul>
+    </ul> --}}
+    <h1>a</h1>
 </x-user-layout>
