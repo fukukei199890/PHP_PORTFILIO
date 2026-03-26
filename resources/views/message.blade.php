acf7ed18672eac1d816226748fa4644080dd6228
nicotinelove <nicotinesukisuki@proton.me>
2026-03-26 10:33:40 +0900

    コメントの追加

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -43,6 +43,7 @@
                                     </div>
                                 </template>
                                 <template x-if="isDeleting">
+                                    {{-- 削除中の処理 --}}
                                     <div class="flex flex-col justify-end">
                                         <div class="bg-blue-500 text-white rounded-2xl px-4 py-2 max-w-xs shadow-sm">
                                             <!-- <p class="text-xs opacity-75 mb-1">あなた</p> -->
