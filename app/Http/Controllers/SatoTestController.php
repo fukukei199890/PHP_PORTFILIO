a5532e395c6c832a90ad503d7b655f2162215afd
nicotinelove <nicotinesukisuki@proton.me>
2026-03-27 13:18:52 +0900

    テストページの更新 ・メッセージ通知の既読処理

--- app/Http/Controllers/SatoTestController.php
+++ app/Http/Controllers/SatoTestController.php
@@ -3,10 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\Message;
 
 class SatoTestController extends Controller
 {
     public function index(){
         return view('satoTest');
+    }
+
+    public function markAsRead(Request $request){
+        // 通知id
+        $notificationId = $request->input('notificationId');
+        // 通知データ
+        $notification = Auth::user()->unreadNotifications->find($notificationId);
+        
+        if(!$notification){
+            // 通知が見つからなかったとき
+            return redirect()->back()->with('message','通知が見つかりませんでした');
+        }
+        if($notification->type == "App\\Notifications\\MessageReceived"){
+            // 通知タイプがメッセージ受け取りの時
+            // 送信者のid
+            $senderId = $notification->data['sender_id'];
+            // 同一のユーザーから送られてきた全ての未読メッセージ通知のインスタンス
+
+            if(!$senderId){
+                // 送信Idがnullのとき
+                return redirect()->back()->with('message','送信者が見つかりませんでした');
+            }
+
+            $notifications = Auth::user()->unreadNotifications->where('type', 'App\\Notifications\\MessageReceived')
+                ->filter(function($n) use ($senderId) {
+                return $n->data['sender_id'] == $senderId;
+            });
+            // 既読処理
+            $notifications->markAsRead();
+
+            // スレッドidの取得
+            $threadId = Message::where('id',$notification->data['message_id'])->first()->thread_id;
+            // セッションに保存
+            session(['current_thread_id'=>$threadId]);
+
+            return redirect()->route('message');
+        }
     }
 }
--- resources/views/satoTest.blade.php
+++ resources/views/satoTest.blade.php
@@ -1,3 +1,29 @@
 <x-user-layout>
-    <h1 class="font-xl">佐藤のテストページです<h1>
+    <h1 class="font-xl">佐藤のテストページです</h1>
+    <p>新着メッセージの通知</p>
+    @php
+        $unreadMessage = Auth::user()->unreadNotifications->where('type', 'App\\Notifications\\MessageReceived');
+    @endphp
+    <ul>
+        @foreach ($unreadMessage as $row)
+            <li>
+                <p>{{ $row }}</p>
+                <p>{{ $row->data['sender_name'] }}からのメッセージです（{{ $row['created_at'] }}）</p>
+                <form method="post" action="{{ route('satoTest.markAsRead') }}">
+                    @csrf
+                    <input type="hidden" name="notificationId" value="{{ $row->id }}">
+                    <button>確認</button>
+                </form>
+            </li>
+        @endforeach
+    </ul>
+    {{-- <p>リクエスト承認の通知
+    @php
+        $unreadMessage = Auth::user()->unreadNotifications->where('type', 'App\\Notifications\\RequestAccepted');
+    @endphp
+    <ul>
+        @foreach ($unreadRequestAccepted as $row)
+            
+        @endforeach
+    </ul> --}}
 </x-user-layout>
